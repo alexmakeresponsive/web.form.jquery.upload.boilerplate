16a9b03543805544724724a7764d6232b5121983
Alexandr Gorchakov <alexmakeresponsive@mail.ru>
2018-09-12 22:01:53 +0600

    ipd

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
 $(document).ready(function (e) {
     $("#test-ajax-upload-form").on('submit',(function(e) {
         e.preventDefault();
-        $.ajax({
-            // sftp://176.57.216.209/home/m/minitiny/public_html/
-            //                url: 'http://minitiny.tmweb.ru/api/amocrm/tilda/heeelp/test-ajax-upload-form.php
+        // $.ajax({
+        //     url: "http://minitiny.tmweb.ru/api/amocrm/tilda/heeelp/test-ajax-upload-form.php",
+        //     type: "POST",
+        //     data:  new FormData(this),
+        //     contentType: false,
+        //     cache: false,
+        //     processData:false,
+            // beforeSend : function()
+        //     {
+        //         //$("#preview").fadeOut();
+        //         $("#err").fadeOut();
+        //     },
+        //     success: function(data)
+        //     {
+        //         if(data=='invalid')
+        //         {
+        //             // invalid file format.
+        //             $("#err").html("Invalid File !").fadeIn();
+        //         }
+        //         else
+        //         {
+        //             $("#err").html(data).fadeIn();
+        //         }
+        //     },
+        //     error: function(e)
+        //     {
+        //         $("#err").html(e).fadeIn();
+        //     }
+        // });
 
+        $.ajax({
+            type: 'POST',
             url: "http://minitiny.tmweb.ru/api/amocrm/tilda/heeelp/test-ajax-upload-form.php",
-            type: "POST",
             data:  new FormData(this),
             contentType: false,
             cache: false,
-            processData:false,
-            beforeSend : function()
-            {
-                //$("#preview").fadeOut();
-                $("#err").fadeOut();
-            },
-            success: function(data)
-            {
-                if(data=='invalid')
-                {
-                    // invalid file format.
-                    $("#err").html("Invalid File !").fadeIn();
-                }
-                else
-                {
-                    // view uploaded file.
-                    $("#preview").html(data).fadeIn();
-                    // $("#form")[0].reset();
-                    $("#err").html("sent success File !").fadeIn();
-                }
-            },
-            error: function(e)
-            {
-                $("#err").html(e).fadeIn();
-            }
-        });
+            processData:false
+        })
+            .done(function(response, textStatus) {
+                console.log('done: response = ', response);
+                console.log('done: textStatus = ', textStatus);
+            })
+            .fail(function(response, textStatus) {
+                console.log('fail: response = ', response);
+                console.log('fail: textStatus = ', textStatus);
+            });
     }));
 });
