206c42027aa094d065081032599048c29ba46192
amr <alexmakeresponsive@gmail.com>
2020-03-23 23:45:36 +0600

    add last changes

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
 $(document).ready(function (e) {
     $("#test-ajax-upload-form").on('submit',(function(e) {
         e.preventDefault();
-        // $.ajax({
-        //     url: "http://minitiny.tmweb.ru/api/amocrm/tilda/heeelp/test-ajax-upload-form.php",
-        //     type: "POST",
-        //     data:  new FormData(this),
-        //     contentType: false,
-        //     cache: false,
-        //     processData:false,
-            // beforeSend : function()
-        //     {
-        //         //$("#preview").fadeOut();
-        //         $("#err").fadeOut();
-        //     },
-        //     success: function(data)
-        //     {
-        //         if(data=='invalid')
-        //         {
-        //             // invalid file format.
-        //             $("#err").html("Invalid File !").fadeIn();
-        //         }
-        //         else
-        //         {
-        //             $("#err").html(data).fadeIn();
-        //         }
-        //     },
-        //     error: function(e)
-        //     {
-        //         $("#err").html(e).fadeIn();
-        //     }
-        // });
+
 
         $.ajax({
-            type: 'POST',
             url: "http://minitiny.tmweb.ru/api/amocrm/tilda/heeelp/test-ajax-upload-form.php",
+            type: 'POST',
             data:  new FormData(this),
             contentType: false,
             cache: false,
             processData:false
         })
             .done(function(response, textStatus) {
-                console.log('done: response = ', response);
-                console.log('done: textStatus = ', textStatus);
+                console.log('done: response = ', JSON.parse(response));
+                // console.log('done: status = ', response);
+                // console.log('done: textStatus = ', textStatus);
             })
             .fail(function(response, textStatus) {
-                console.log('fail: response = ', response);
-                console.log('fail: textStatus = ', textStatus);
+                console.log('fail: response = ', response.responseText);
+                console.log('fail: status = ', response.status);
+                // console.log('fail: textStatus = ', textStatus);
             });
     }));
 });
